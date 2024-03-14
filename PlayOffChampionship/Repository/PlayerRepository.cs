8fbd4903bb88b0aea6c8536d194e117b07f69d22
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2024-03-14 14:36:56 +0000

    added implementation for player get, create and delete repository methods

---
+++ PlayOffChampionship/Repository/PlayerRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlayOffChampionship.Interfaces;
+using PlayOffChampionship.Models;
+
+namespace PlayOffChampionship.Repository
+{
+    public class PlayerRepository : IPlayerRepository
+    {
+
+        private readonly AppDbContext _context;
+        public PlayerRepository(AppDbContext context) 
+        { 
+            _context = context;
+        }
+
+        public async Task<Player> Create(Player player)
+        {
+            _context.Players.Add(player);
+
+            await _context.SaveChangesAsync();
+
+            return player;
+        }
+
+        public async Task<Player?> Delete(int id)
+        {
+            var player = await _context.Players.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (player == null)
+            {
+                return null;
+            }
+
+            _context.Players.Remove(player);
+
+            await _context.SaveChangesAsync();
+
+            return player;
+        }
+
+        public async Task<List<Player>> GetAllPlayers()
+        {
+            return await _context.Players.ToListAsync();
+        }
+
+        public async Task<Player?> GetPlayerById(int id)
+        {
+            return await _context.Players.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public Task<Player?> Update(int id, Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
